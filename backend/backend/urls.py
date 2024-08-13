620d11fafa672191550deb181056abd6a9af8634
sujanale10 <sujanale404@gmail.com>
2024-08-13 21:24:59 +1000

    changes in interface

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-
+# AWS RDS credentials
+# master username: admin
+# master password: Helloworld!123
 urlpatterns = [
     path('admin/', admin.site.urls), # username: admin password: Helloworld@123
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True)))
