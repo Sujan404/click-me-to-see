c0040622b037dc213c0491a6480499910645dfb8
sujanale10 <sujanale404@gmail.com>
2024-09-03 11:01:03 +1000

    add create post button

--- frontend/frontend/src/views/main/Post.vue
+++ frontend/frontend/src/views/main/Post.vue
@@ -1,148 +1,146 @@
 <template>
-    <div class="home">
-      <div>
-      <p>Click the button to create post</p>
-      <button>Create Post</button>
+  <div class="home">
+    <div class="flex justify-end mt-2">
+      <button class="bg-blue-500 text-white hover:text-stone-950 hover:bg-amber-500 p-2 rounded-md">Create Post</button>
     </div>
-        <div class="flex flex-col place-items-center border-b-2">
-            <img :src="'/uploads/' + this.postBySlug.featuredImage" class="w-full my-5" />
-            <h1 class="text-center text-5xl font-extrabold mb-5">{{ this.postBySlug.title || null}}</h1>
-            <p class="text-gray-500 text-lg mb-2">{{ formatDate(this.postBySlug.createdAt) }} - By {{
-                this.postBySlug.user.username }}</p>
-        </div>
 
-        <div class="flex flex-wrap my-4">
-            <div class="mr-5 text-sm font-medium"> Tags:</div>
-            <router-link v-for="tag in this.postBySlug.tag" :key="tag.name"
-                class="mr-5 text-sm font-medium uppercase text-teal-500 hover:underline hover:text-teal-7000"
-                :to="`/tag/${tag.slug}`">
-                {{ tag.name }}
-            </router-link>
-        </div>
-
-        <!----Main content-->
-        <div class="py-5 font-serif space-y-4">
-            <!-- <div v-html="this.postBySlug.content"></div> -->
-        </div>
-
-        <!---Like, Comment and Share-->
-        <div class="flex flex-wrap py-4 space-x-8 justify-center items-center text-xl">
-            <div v-if="this.liked === true" @click="this.updateLike()">
-                <i class="fa-solid fa-thumbs-up">
-                    <span class="font-sans font-semibold ml-1">
-                        {{ this.numberOfLikes }}
-                    </span>
-                </i>
-            </div>
-            <div v-else @click="this.updateLike()">
-                <i class="fa-solid fa-thumbs-up">
-                    <span class="font-sans font-semibold ml-1">
-                        {{ this.numberOfLikes }}
-                    </span>
-                </i>
-            </div>
-            <div @click="this.toggleCommentSection()">
-                <i class="fa-regular fa-comment-dots">
-                    <span class="font-sans font-semibold ml-1">
-                        {{ this.numberOfApprovedComments }}</span>
-                </i>
-            </div>
-            <div id="socialShareIcon" @click="this.toggleSocialSharePopover()">
-                <i class="fa-solid fa-share-nodes"></i>
-            </div>
-            <div id="socialShare">
-                <div v-if="this.showSocialShare" class="flex space-x-2 drop-shadow-lg border-2 p-2">
-                    <i class="fa-brands fa-linkedin text-3xl text-gray-700 hover:text-teal-700"></i>
-                    <i class="fa-brands fa-facebook-square text-3xl text-gray-700 hover:text-teal-700"></i>
-                    <i class="fa-brands fa-twitter-square text-3xl text-gray-700 hover:text-teal-700"></i>
-                    <i class="fa-brands fa-google-plus-square text-3xl text-gray-700 hover:text-teal-700"></i>
-                    <i class="fa-brands fa-github-square text-3xl text-gray-700 hover:text-teal-700"></i>
-                    <i class="fa-brands fa-dev text-3xl text-gray-700 hover:text-teal-700"></i>
-                </div>
-            </div>
-        </div>
-
-        <!--Comment Section-->
-        <!--Pass the approved comments and the post id to the comment component-->
-        <comment-section-component
-        v-if="this.approvedComments && this.showComment"
-        :comments="this.approvedComments"
-        :postID="this.postBySlug.id"
-        :userID="this.userID"></comment-section-component>
+    
+    <div class="flex flex-col place-items-center border-b-2">
+      <img :src="'/uploads/' + this.postBySlug.featuredImage" class="w-full my-5" />
+      <h1 class="text-center text-5xl font-extrabold mb-5">{{ this.postBySlug.title || null }}</h1>
+      <p class="text-gray-500 text-lg mb-2">{{ formatDate(this.postBySlug.createdAt) }} - By {{
+        this.postBySlug.user.username }}</p>
     </div>
+
+    <div class="flex flex-wrap my-4">
+      <div class="mr-5 text-sm font-medium"> Tags:</div>
+      <router-link v-for="tag in this.postBySlug.tag" :key="tag.name"
+        class="mr-5 text-sm font-medium uppercase text-teal-500 hover:underline hover:text-teal-7000"
+        :to="`/tag/${tag.slug}`">
+        {{ tag.name }}
+      </router-link>
+    </div>
+
+    <!----Main content-->
+    <div class="py-5 font-serif space-y-4">
+      <!-- <div v-html="this.postBySlug.content"></div> -->
+    </div>
+
+    <!---Like, Comment and Share-->
+    <div class="flex flex-wrap py-4 space-x-8 justify-center items-center text-xl">
+      <div v-if="this.liked === true" @click="this.updateLike()">
+        <i class="fa-solid fa-thumbs-up">
+          <span class="font-sans font-semibold ml-1">
+            {{ this.numberOfLikes }}
+          </span>
+        </i>
+      </div>
+      <div v-else @click="this.updateLike()">
+        <i class="fa-solid fa-thumbs-up">
+          <span class="font-sans font-semibold ml-1">
+            {{ this.numberOfLikes }}
+          </span>
+        </i>
+      </div>
+      <div @click="this.toggleCommentSection()">
+        <i class="fa-regular fa-comment-dots">
+          <span class="font-sans font-semibold ml-1">
+            {{ this.numberOfApprovedComments }}</span>
+        </i>
+      </div>
+      <div id="socialShareIcon" @click="this.toggleSocialSharePopover()">
+        <i class="fa-solid fa-share-nodes"></i>
+      </div>
+      <div id="socialShare">
+        <div v-if="this.showSocialShare" class="flex space-x-2 drop-shadow-lg border-2 p-2">
+          <i class="fa-brands fa-linkedin text-3xl text-gray-700 hover:text-teal-700"></i>
+          <i class="fa-brands fa-facebook-square text-3xl text-gray-700 hover:text-teal-700"></i>
+          <i class="fa-brands fa-twitter-square text-3xl text-gray-700 hover:text-teal-700"></i>
+          <i class="fa-brands fa-google-plus-square text-3xl text-gray-700 hover:text-teal-700"></i>
+          <i class="fa-brands fa-github-square text-3xl text-gray-700 hover:text-teal-700"></i>
+          <i class="fa-brands fa-dev text-3xl text-gray-700 hover:text-teal-700"></i>
+        </div>
+      </div>
+    </div>
+
+    <!--Comment Section-->
+    <!--Pass the approved comments and the post id to the comment component-->
+    <comment-section-component v-if="this.approvedComments && this.showComment" :comments="this.approvedComments"
+      :postID="this.postBySlug.id" :userID="this.userID"></comment-section-component>
+  </div>
 </template>
 
 <script>
-import {POST_BY_SLUG} from "@/queries";
+import { POST_BY_SLUG } from "@/queries";
 import CommentSectionComponent from "@/components/CommentSection.vue";
-import {UPDATE_POST_LIKE} from "@/mutations";
-import {createPopper} from "@popperjs/core";
+import { UPDATE_POST_LIKE } from "@/mutations";
+import { createPopper } from "@popperjs/core";
 
 export default {
-    name: "PostView",
-    components:{CommentSectionComponent},
+  name: "PostView",
+  components: { CommentSectionComponent },
 
-    data(){
-        return {
-          // this is temporary
-            postBySlug: {
-              title: null,
-              featuredImage: null,
-              user:{
-                username:null
-              }
-            },
-            // postBySlug: null, // this is original needs to check later
-            comments:null,
-            liked:false,
-            numberOfLikes:0,
-            userID: null,
-            showComment: false,
-            showSocialShare:false
+  data() {
+    return {
+      // this is temporary
+      postBySlug: {
+        title: null,
+        featuredImage: null,
+        user: {
+          username: null
         }
+      },
+      // postBySlug: null, // this is original needs to check later
+      comments: null,
+      liked: false,
+      numberOfLikes: 0,
+      userID: null,
+      showComment: false,
+      showSocialShare: false
+    }
+  },
+  computed: {
+    approvedComments() {
+      // return this.comments.filter((comment) => comment.isApproved);
     },
-    computed:{
-        approvedComments(){
-            // return this.comments.filter((comment) => comment.isApproved);
-        },
-        numberOfApprovedComments(){
-            // return Objects.keys(this.approvedComments).length;
+    numberOfApprovedComments() {
+      // return Objects.keys(this.approvedComments).length;
+    }
+  },
+
+  async created() {
+    try {
+      const post = await this.$apollo.query({
+        query: POST_BY_SLUG,
+        variables: {
+          // slug:this.$route.params.slug,
+          slug: "hello"
         }
-    },
+      });
+      console.log("asfasdfasdfasdf")
+      this.postBySlug = post.data.postBySlug;
+      this.comments = post.data.postBySlug.commentSet;
 
-    async created(){
-      try{
-        const post = await this.$apollo.query({
-            query:POST_BY_SLUG,
-            variables:{
-                // slug:this.$route.params.slug,
-                slug:"hello"
-            }
-        });
-        console.log("asfasdfasdfasdf")
-        this.postBySlug = post.data.postBySlug;
-        this.comments = post.data.postBySlug.commentSet;
+      // Check if the current user has liked the post
+      // Get the current user id
+      this.userID = JSON.parse(localStorage.getItem("user")).id;
 
-        // Check if the current user has liked the post
-        // Get the current user id
-        this.userID = JSON.parse(localStorage.getItem("user")).id;
+      // Find if the current user has liked the post
+      let likedUsers = this.postBySlug.likes;
 
-        // Find if the current user has liked the post
-        let likedUsers = this.postBySlug.likes;
-
-        for(let likedUser in likedUsers){
-            if(likedUsers[likedUser].id === this.userID){
-                this.liked = true;
-            }
+      for (let likedUser in likedUsers) {
+        if (likedUsers[likedUser].id === this.userID) {
+          this.liked = true;
         }
-
-        // Get the number of likes
-        this.numberOfLikes = parseInt(this.postBySlug.numberOfLikes)
-      }catch(error){
-        console.log(error.message)
       }
-    },
-    mounted() {
+
+      // Get the number of likes
+      this.numberOfLikes = parseInt(this.postBySlug.numberOfLikes)
+    } catch (error) {
+      console.log(error.message)
+    }
+  },
+  mounted() {
     const socialShareIcon = document.querySelector("#socialShareIcon");
     const socialShare = document.querySelector("#socialShare");
     createPopper(socialShareIcon, socialShare, {
