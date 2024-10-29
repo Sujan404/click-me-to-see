aa8975ddc41a96835ab61d9ab6918ac8b5fe2c6a
sujanale10 <sujanale404@gmail.com>
2024-10-30 02:00:04 +1100

    working in frontend style

--- frontend/frontend/src/views/home/AboutMe.vue
+++ frontend/frontend/src/views/home/AboutMe.vue
@@ -83,8 +83,8 @@
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CSS Flex importance
                     </h5>
                 </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Bastion Host architeture provides secured
-                    access to resources in a private subnet without exposing them directly to the internet</p>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">CSS Flexbox is essential for creating flexible, responsive layouts that adjust seamlessly across
+                    different screen sizes and devices.</p>
                 <router-link :to="{ name: 'CssFlex' }"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
